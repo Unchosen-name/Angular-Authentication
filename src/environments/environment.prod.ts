865e19e977791426b74aac5292a43a231309b17a
Darryl Wu <darryl.wu301@gmail.com>
2020-06-13 00:11:37 -0400

    Remove API Key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,8 @@
 export const environment = {
   production: true,
   firebase:{
-    apiKey: "AIzaSyDGVmtRhiiUdOfZK2kSIedYxwespKK8j3Q",
-    authDomain: "pencilmediumdocs.firebaseapp.com",
-    databaseURL: "https://pencilmediumdocs.firebaseio.com",
-    projectId: "pencilmediumdocs",
-    storageBucket: "pencilmediumdocs.appspot.com",
-    messagingSenderId: "57072458519",
-    appId: "1:57072458519:web:0676b5355a924e12bbb552",
-    measurementId: "G-9ZKVYQFQPG"
+
+    //INSERT API KEY AND RELATIVE INFORMATION
+
   },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,9 @@
 export const environment = {
   production: false,
   firebase:{
-    apiKey: "AIzaSyDGVmtRhiiUdOfZK2kSIedYxwespKK8j3Q",
-    authDomain: "pencilmediumdocs.firebaseapp.com",
-    databaseURL: "https://pencilmediumdocs.firebaseio.com",
-    projectId: "pencilmediumdocs",
-    storageBucket: "pencilmediumdocs.appspot.com",
-    messagingSenderId: "57072458519",
-    appId: "1:57072458519:web:0676b5355a924e12bbb552",
-    measurementId: "G-9ZKVYQFQPG"
+
+    //INSERT API KEY AND RELATIVE INFORMATION
+
   },
 };
 
